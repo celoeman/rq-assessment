e68b03ba8d15fdd5c4d578b2673c642a4041c2a5
Eman Celo <celoeman@gmail.com>
2024-11-01 17:53:02 -0400

    udpate gql query to grab additional fields;

--- src/hooks/useGetPokemons.ts
+++ src/hooks/useGetPokemons.ts
@@ -2,42 +2,96 @@ import { useMemo } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
-export type Pokemon = {
-  id: string;
-  name: string;
+export type PokemonType = {
+    id: string;
+    name: string;
+    types: string[];
+    number: string;
+    weight: any;
+    height: any;
+    classification: string;
+    resistant: string[];
+    weaknesses: string[];
+    attacks: any;
+    fleeRate: number;
+    maxCP: number;
+    evolutions: string[];
+    evolutionRequirements: {
+        amount: number;
+        name: string;
+    };
+    maxHP: number;
+    image: string;
 };
 
 export type PokemonOption = {
-  value: Pokemon['id'];
-  label: Pokemon['name'];
+    value: PokemonType['id'];
+    label: PokemonType['name'];
 };
 
 export const GET_POKEMONS = gql`
-  query pokemons($first: Int!) {
-    pokemons(first: $first) {
-      id
-      name
+    query pokemons($first: Int!) {
+        pokemons(first: $first) {
+            id
+            name
+            image
+            types
+            number
+            classification
+            resistant
+            weaknesses
+            fleeRate
+            maxCP
+            maxHP
+            evolutions {
+                name
+            }
+            evolutionRequirements {
+                amount
+                name
+            }
+            weight {
+                minimum
+                maximum
+            }
+            height {
+                minimum
+                maximum
+            }
+            attacks {
+                fast {
+                    name
+                    type
+                    damage
+                }
+                special {
+                    name
+                    type
+                    damage
+                }
+            }
+        }
     }
-  }
 `;
 
 export const useGetPokemons = () => {
-  const { data, ...queryRes } = useQuery(GET_POKEMONS, {
-    variables: {
-      first: 151, // Keep hard coded
-    },
-  });
+    const { data, ...queryRes } = useQuery(GET_POKEMONS, {
+        variables: {
+            first: 151, // Keep hard coded
+        },
+    });
 
-  const pokemons: Pokemon[] = useMemo(() => data?.pokemons || [], [data]);
+    const pokemons: PokemonType[] = useMemo(() => data?.pokemons || [], [data]);
 
-  const pokemonOptions: PokemonOption[] = useMemo(
-    () => pokemons.map((p: Pokemon) => ({ value: p.id, label: p.name })),
-    [pokemons]
-  );
+    const pokemonOptions: PokemonOption[] = useMemo(
+        () =>
+            pokemons.map((p: PokemonType) => ({ value: p.id, label: p.name })),
+        [pokemons]
+    );
 
-  return {
-    pokemons,
-    pokemonOptions,
-    ...queryRes,
-  };
+    return {
+        pokemons,
+        pokemonOptions,
+        ...queryRes,
+    };
 };
